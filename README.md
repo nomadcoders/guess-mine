4b9bde0cf01d99f9b9b2725c12c9b66e318a3fa9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-09 16:48:54 +0900

    #1.1 Server Setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Guess Mine
+
 Realtime Drawing Game built with SocketIO, Gulp and Node
+
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [ ] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 4000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
