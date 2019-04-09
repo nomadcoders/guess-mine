11eff145584dd49ea5afa9f7bfbe4cd21ff77da7
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-09 17:14:17 +0900

    #1.4 Setting Up SocketIO

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Realtime Drawing Game built with SocketIO, Gulp and Node
 - [x] Server
 - [x] Pug
 - [x] Static
+- [x] ESLint
 - [ ] SocketIO
--- src/server.js
+++ src/server.js
@@ -12,4 +12,6 @@ app.get("/", (req, res) => res.render("home"));
 const handleListening = () =>
   console.log(`✅ Server running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
