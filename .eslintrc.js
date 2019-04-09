323fe4bc8b3c95994971fbedff90d4f657e92abe
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-09 17:14:06 +0900

    #1.2 ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    "no-console": "off"
+  }
+};
