54daafda0815b10dccb7271fde344eba76109b48
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-10 14:27:57 +0900

    #3.1 Gulp is Awesome part Two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,36 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+
+sass.compiler = require("node-sass");
 
 const paths = {
   styles: {
     src: "assets/scss/styles.scss",
-    dest: "src/static/styles"
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss"
   }
 };
 
-export function styles() {
+function styles() {
   return gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
