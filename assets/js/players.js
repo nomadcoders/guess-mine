dbce318be73f5d18530067c62a967ecf3bc42bde
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-11 16:31:24 +0900

    #6.3 Game Programming part One

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,1 +1,12 @@
-export const handlePlayerUpdate = ({ sockets }) => console.log(sockets);
+const board = document.getElementById("jsPBoard");
+
+const addPlayers = players => {
+  board.innerHTML = "";
+  players.forEach(player => {
+    const playerElement = document.createElement("span");
+    playerElement.innerText = `${player.nickname}: ${player.points}`;
+    board.appendChild(playerElement);
+  });
+};
+
+export const handlePlayerUpdate = ({ sockets }) => addPlayers(sockets);
--- src/socketController.js
+++ src/socketController.js
@@ -1,19 +1,31 @@
 import events from "./events";
-import { setInterval } from "timers";
+import { chooseWord } from "./words";
 
 let sockets = [];
+let inProgress = false;
+let word = null;
+
+const chooseLeader = () => sockets[Math.floor(Math.random() * sockets.length)];
 
 const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
   const superBroadcast = (event, data) => io.emit(event, data);
   const sendPlayerUpdate = () =>
     superBroadcast(events.playerUpdate, { sockets });
+  const startGame = () => {
+    if (inProgress === false) {
+      inProgress = true;
+      const leader = chooseLeader();
+      word = chooseWord();
+    }
+  };
 
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
     sendPlayerUpdate();
+    startGame();
   });
 
   socket.on(events.disconnect, () => {
